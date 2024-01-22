95247c8d79dabdff30e1c872bf96059f83a2ae7c
unknown <letamhot@gmail.com>
2024-01-22 10:53:58 +0700

    abc

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://amibakeshop-29b184a12536.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
--- config/database.php
+++ config/database.php
@@ -66,10 +66,10 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'https://amibakeshop-29b184a12536.herokuapp.com/'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', 'amibakeshop'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
