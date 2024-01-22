808fa721aad6e33f01c52825a1381b089fb73638
unknown <letamhot@gmail.com>
2024-01-22 13:57:30 +0700

    update

--- database/seeders/ProducersTableSeeder.php
+++ database/seeders/ProducersTableSeeder.php
@@ -21,19 +21,6 @@ class ProducersTableSeeder extends Seeder
         \DB::table('producers')->insert(array (
             0 => 
             array (
-                'id' => 1,
-                'name' => 'Gundam shop Huế',
-                'slug' => 'gundam-shop-hue1618382933',
-                'address' => 'Hồ Đắc Di, Tp Huế',
-                'phone' => '3934842223',
-                'taxCode' => 1331213123121,
-                'image' => 'imageProducer/10_vclub_1618382933.png',
-                'created_at' => '2021-04-14 06:48:53',
-                'updated_at' => '2021-04-25 14:32:14',
-                'deleted_at' => '2021-04-25 14:32:14',
-            ),
-            1 => 
-            array (
                 'id' => 2,
                 'name' => 'AMIBAKESHOP',
                 'slug' => 'amibakeshop1705648670',
@@ -44,32 +31,6 @@ class ProducersTableSeeder extends Seeder
                 'created_at' => '2021-04-25 14:32:37',
                 'updated_at' => '2024-01-19 07:17:50',
                 'deleted_at' => NULL,
-            ),
-            2 => 
-            array (
-                'id' => 3,
-                'name' => 'Daban',
-                'slug' => 'daban1619361213',
-                'address' => 'Thành phố Hồ Chí Minh',
-                'phone' => '0913131222',
-                'taxCode' => 2342343242343,
-                'image' => 'imageProducer/daban_1619361213.jpg',
-                'created_at' => '2021-04-25 14:33:33',
-                'updated_at' => '2024-01-19 07:15:56',
-                'deleted_at' => '2024-01-19 07:15:56',
-            ),
-            3 => 
-            array (
-                'id' => 4,
-                'name' => 'Super Nova',
-                'slug' => 'super-nova1619361261',
-                'address' => 'Thành phố Hồ Chí Minh',
-                'phone' => '0123456781',
-                'taxCode' => 13144212232211,
-                'image' => 'imageProducer/super_nova_1619361261.jpg',
-                'created_at' => '2021-04-25 14:34:21',
-                'updated_at' => '2024-01-19 07:15:52',
-                'deleted_at' => '2024-01-19 07:15:52',
             ),
         ));
         
--- database/seeders/ProductsTableSeeder.php
+++ database/seeders/ProductsTableSeeder.php
@@ -21,24 +21,6 @@ class ProductsTableSeeder extends Seeder
         \DB::table('products')->insert(array (
             0 => 
             array (
-                'id' => 1,
-                'name' => 'Gamdum',
-                'slug' => 'gamdum1618388986',
-                'id_category' => 1,
-                'id_producer' => 1,
-                'amount' => 12,
-                'image' => 'imageProduct/121459708_403251084178097_1329374978001414759_n_1618382964.jpg',
-                'image1' => 'imageProduct/121648553_784632542389446_3566435560311680692_n_1618382964.jpg',
-                'image2' => 'imageProduct/10_vclub_1618382964.png',
-                'price' => 21000,
-                'new' => 1,
-                'description' => 'Helllo',
-                'created_at' => '2021-04-14 06:49:24',
-                'updated_at' => '2021-04-25 14:30:29',
-                'deleted_at' => '2021-04-25 14:30:29',
-            ),
-            1 => 
-            array (
                 'id' => 2,
                 'name' => 'Bánh bông lan trứng muối',
                 'slug' => 'banh-bong-lan-trung-muoi1705653620',
@@ -56,117 +38,7 @@ Nguyên liệu: Bánh bông lan thơm ngon, trứng muối và sợi ruốc',
                 'updated_at' => '2024-01-19 08:40:20',
                 'deleted_at' => NULL,
             ),
-            2 => 
-            array (
-                'id' => 3,
-                'name' => 'Daban Mô Hình Gundam HG Shiranui Akatsuki',
-                'slug' => 'daban-mo-hinh-gundam-hg-shiranui-akatsuki1619539253',
-                'id_category' => 2,
-                'id_producer' => 3,
-                'amount' => 20,
-                'image' => 'imageProduct/6ed59eeccd5ee8b39d69d3ca88462036_copy_copy_copy_1619361839.jpg',
-                'image1' => 'imageProduct/6ed59eeccd5ee8b39d69d3ca88462036_copy_copy_copy_copy_copy_1619361839.jpg',
-                'image2' => 'imageProduct/400_1619361839.jpg',
-                'price' => 1800000,
-                'new' => 1,
-                'description' => 'Tỷ Lệ : 1:144
-
-Độ Tuổi : >14
-
-Phân Loại Sp : Lắp Ráp
-
-Sản Phẩm Nhựa Cao Cấp Với Độ Sắc Nét Cao
-Sản Xuất Bởi Bandai Namco – Nhật Bản
-An Toàn Với Trẻ Em
-Phát Triển Trí Não Cho Trẻ Hiệu Quả Đi Đôi Với Niềm Vui Thích Bất Tận
-Rèn Luyện Tính Kiên Nhẫn Cho Người Chơi',
-                'created_at' => '2021-04-25 14:43:59',
-                'updated_at' => '2024-01-19 07:32:43',
-                'deleted_at' => '2024-01-19 07:32:43',
-            ),
-            3 => 
-            array (
-                'id' => 4,
-                'name' => 'Mô Hình Gundam HG X Maoh',
-                'slug' => 'mo-hinh-gundam-hg-x-maoh1619539264',
-                'id_category' => 2,
-                'id_producer' => 3,
-                'amount' => 20,
-                'image' => 'imageProduct/hg_1619361918.jpg',
-                'image1' => 'imageProduct/hg_gundam_marchosias_4_1619361918.jpg',
-                'image2' => 'imageProduct/hg_gundam_marchosias_box_art_1619361918.jpg',
-                'price' => 1800000,
-                'new' => 1,
-                'description' => 'Tỷ Lệ : 1:144
-
-Độ Tuổi : >14
-
-Phân Loại Sp : Lắp Ráp
-
-Sản Phẩm Nhựa Cao Cấp Với Độ Sắc Nét Cao
-Sản Xuất Bởi Bandai Namco – Nhật Bản
-An Toàn Với Trẻ Em
-Phát Triển Trí Não Cho Trẻ Hiệu Quả Đi Đôi Với Niềm Vui Thích Bất Tận
-Rèn Luyện Tính Kiên Nhẫn Cho Người Chơi',
-                'created_at' => '2021-04-25 14:45:18',
-                'updated_at' => '2024-01-19 07:32:52',
-                'deleted_at' => '2024-01-19 07:32:52',
-            ),
-            4 => 
-            array (
-                'id' => 5,
-                'name' => 'Daban 6619S Mô Hình Gundam MG Nu Ver Ka Titanium Finish',
-                'slug' => 'daban-6619s-mo-hinh-gundam-mg-nu-ver-ka-titanium-finish1619539246',
-                'id_category' => 3,
-                'id_producer' => 3,
-                'amount' => 21,
-                'image' => 'imageProduct/20201229215019_800x800_1619417328.jpg',
-                'image1' => 'imageProduct/20201229215100_800x800_1619361993.jpg',
-                'image2' => 'imageProduct/20201229215052_800x800_1619417328.jpg',
-                'price' => 9500000,
-                'new' => 1,
-                'description' => 'Tỷ Lệ : 1:100
-
-Độ Tuổi : >14
-
-Phân Loại Sp : Lắp Ráp
-
-Sản Phẩm Nhựa Cao Cấp Với Độ Sắc Nét Cao
-Sản Xuất Bởi Bandai Namco – Nhật Bản
-An Toàn Với Trẻ Em
-Phát Triển Trí Não Cho Trẻ Hiệu Quả Đi Đôi Với Niềm Vui Thích Bất Tận
-Rèn Luyện Tính Kiên Nhẫn Cho Người Chơi',
-                'created_at' => '2021-04-25 14:46:33',
-                'updated_at' => '2024-01-19 07:32:39',
-                'deleted_at' => '2024-01-19 07:32:39',
-            ),
-            5 => 
-            array (
-                'id' => 6,
-                'name' => 'Gundam Bandai PG RX-78-2 2020',
-                'slug' => 'gundam-bandai-pg-rx-78-2-20201619362558',
-                'id_category' => 4,
-                'id_producer' => 2,
-                'amount' => 21,
-                'image' => 'imageProduct/10729406b_1619362558.jpg',
-                'image1' => 'imageProduct/10729406b2_1619362558.jpg',
-                'image2' => 'imageProduct/rx78_1619362558.jpg',
-                'price' => 6000000,
-                'new' => 1,
-                'description' => 'Sản phẩm PG mới nhất sẽ có các đặc điểm sau :
-SP là thành quả kết tinh của các công nghệ sản xuất mô hình mới nhất của Bandai
-Frame ( khung xương ) có kích thước chiều dài chân tới 18cm
-Hơn 90 góc cử động ( số lượng cao nhất trong lịch sử thiết kế Gundam Bandai ), đi kèm khả năng khóa khớp.
-Nhiều chi tiết có màu kim loại ( tổng cộng 4 mã màu kim loại )
-Sử dụng nhiều chất màu trong sản phẩm : bạc, mờ, chrome bóng, metal part, 1 số chi tiết kim loại ốp lên frame.
-Là mô hình có nhiều chi tiết open-hatch , gimmick nhất trong lịch sử Gundam.
-Trang bị đèn LED có khả năng thay đổi màu, đèn led được thiết kế để vừa tăng độ thẩm mỹ nhưng vẫn đảm bảo biên độ cử động là tốt nhất.
-Kèm đèn LED trong Beam saber ( kiếm năng lượng )',
-                'created_at' => '2021-04-25 14:55:58',
-                'updated_at' => '2024-01-19 07:32:47',
-                'deleted_at' => '2024-01-19 07:32:47',
-            ),
-            6 => 
+            1 => 
             array (
                 'id' => 7,
                 'name' => 'Trà sữa truyền thống',
@@ -185,7 +57,7 @@ Nguyên liệu: Trà xanh chắt lấy nước, sữa tươi, chân châu đen, 
                 'updated_at' => '2024-01-19 08:31:58',
                 'deleted_at' => NULL,
             ),
-            7 => 
+            2 => 
             array (
                 'id' => 8,
                 'name' => 'Trà sữa kem trứng',
@@ -204,7 +76,7 @@ Nguyên liệu: Nước trà xanh chắt lọc, kem trứng, sữa tươi, dừa
                 'updated_at' => '2024-01-19 08:33:38',
                 'deleted_at' => NULL,
             ),
-            8 => 
+            3 => 
             array (
                 'id' => 9,
                 'name' => 'Su kem',
@@ -223,7 +95,7 @@ Nguyên liệu: Bánh nướng, kem trứng',
                 'updated_at' => '2024-01-19 08:33:27',
                 'deleted_at' => NULL,
             ),
-            9 => 
+            4 => 
             array (
                 'id' => 10,
                 'name' => 'Bánh kem nhiều mẫu size vừa',
@@ -242,7 +114,7 @@ Nguyên liệu: Bánh nướng, kem phối nhiều màu không phụ gia, lớp 
                 'updated_at' => '2024-01-19 08:40:41',
                 'deleted_at' => NULL,
             ),
-            10 => 
+            5 => 
             array (
                 'id' => 11,
                 'name' => 'Bánh kem nhiều mẫu size lớn',
