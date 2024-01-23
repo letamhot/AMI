635d7f67eb1da7f0a27562932b376ec43c2f8b51
unknown <letamhot@gmail.com>
2024-01-23 09:18:11 +0700

    update

--- resources/views/page/product-detail.blade.php
+++ resources/views/page/product-detail.blade.php
@@ -161,9 +161,28 @@
                                     @endif
                                 @endif
                                 </div>
-                                <span class="btn btn-primary proceed-btn" style="width: 40%">
-                                    <a href="0822175675"><i class="fa fa-phone" aria-hidden="true"></i>Liên hệ: 0822175675</a>
-                                </span>
+                                <button type="button" data-toggle="modal" data-target="#exampleModal">
+                                    <i class="fa fa-phone" aria-hidden="true"></i>Xem liên hệ
+                                  </button>
+                                  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                      <div class="modal-content">
+                                        <div class="modal-header">
+                                          <h5 class="modal-title" id="exampleModalLabel">Gọi điện thoại</h5>
+                                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                          </button>
+                                        </div>
+                                        <div class="modal-body">
+                                          <p>Liên hệ: 0822175675</p>
+                                        </div>
+                                        <div class="modal-footer">
+                                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Đóng</button>
+                                          <button type="button" class="btn btn-primary">Gọi</button>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
                                 <input type="hidden" id="check_stock" name="check_stock"
                                     value="{{ $id_product->amount }}" style="display: flex">
                                 <p style="padding-top: 20px;" id="quantity" name="qty"><b>Số lượng:</b>
